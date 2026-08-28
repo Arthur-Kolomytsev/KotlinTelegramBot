80f66d4bd6c627ba513cffaa34ab5572da5475de
Artur <artur.kolomytsev@mail.ru>
2026-08-28 18:53:45 +0300

    KTB-02 read lines from file to console

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,11 +1,18 @@
 package org.example
 
 import java.io.File
+import java.io.FileNotFoundException
 
 
 fun main() {
 
     val words = File("words.txt")
-    words.readLines().forEach { println(it) }
+
+    try {
+        words.readLines().forEach { println(it) }
+
+    } catch (e: FileNotFoundException) {
+        println("Файл не найден: ${e.message}")
+    }
 
 }
